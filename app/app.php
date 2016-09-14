0abb033938e91a91ee674fad8a2a138e5f37ccdd
Angie Smith and Timothy Bourgault <angie@>
2016-09-14 16:49:25 -0700

    blah the blah blab.

--- app/app.php
+++ app/app.php
@@ -20,19 +20,28 @@
 
 
 
-    $app->get("/", function() use ($app) {
+    $app->get("/", function() use($app) {
         return $app['twig']->render('game.html.twig');
     });
 
     $app->post("/view_game", function() use($app) {
         $input1 = $_POST['input1'];
         $input2 = $_POST['input2'];
+
         $new_game = new Game;
-        $new_game->save();
 
         $match = $new_game->playGame($input1, $input2);
+        $new_game->save();
 
-        return $app['twig']->render('game_results.html.twig', array('match' => $match, 'input1' => $input1, 'input2' => $input2));
+        return $app['twig']->render('game_results.html.twig', array('match' => $match, 'input1' => $input1, 'input2' => $input2, 'player1Win' => $new_game->player1Win, 'player2Win' => $new_game->player2Win));
+    });
+
+    $app->get("/game", function() use($app) {
+        $game = Game::getAll();
+        $match = $game->playGame($input1, $input2);
+        $game->save();
+
+        return $app['twig']->render('game_results.html.twig', array('match' => $match, 'input1' => $input1, 'input2' => $input2, 'player1Win' => $game->player1Win, 'player2Win' => $game->player2Win));
     });
     return $app
 ?>
--- src/Roshambo.php
+++ src/Roshambo.php
@@ -4,37 +4,40 @@
         public $player1Win;
         public $player2Win;
 
-        function __construct()
-        {
-            $this->player1Win = 0;
-            $this->player2Win = 0;
-        }
+        // function __construct()
+        // {
+        //     $this->player1Win = 0;
+        //     $this->player2Win = 0;
+        // }
 
         function playGame($input1, $input2)
         {
             $message = "";
 
+            $input1 = strtolower($input1);
+            $input2 = strtolower($input2);
+
             if ($input1 == $input2)
             {
                 return "It's a DRAW!";
-            } elseif ($input1 == "Rock" && $input2 == "Scissors" ||
-                    $input1 == "Paper" && $input2 == "Rock" ||
-                    $input1 == "Scissors" && $input2 == "Paper") {
-                // $player1Win++;
+            } elseif ($input1 == "rock" && $input2 == "scissors" ||
+                    $input1 == "paper" && $input2 == "rock" ||
+                    $input1 == "scissors" && $input2 == "paper") {
+                $this->player1Win++;
                 $message = "Player 1 Wins the Freakin Match!!!";
-            } elseif ($input2 == "Rock" && $input1 == "Scissors" ||
-                    $input2 == "Paper" && $input1 == "Rock" ||
-                    $input2 == "Scissors" && $input1 == "Paper"){
-                // $player2Win++;
+            } elseif ($input2 == "rock" && $input1 == "scissors" ||
+                    $input2 == "paper" && $input1 == "rock" ||
+                    $input2 == "scissors" && $input1 == "paper"){
+                $this->player2Win++;
                 $message = "Player 2 Wins the Freakin Match!!!";
             }
 
-            // if ($player1Win == 2)
-            // {
-            //     $message = "Player 1 is the Super Duper Freakin winner of the WHOLE ENTIRE Game!";
-            // } elseif ($player2Win == 2) {
-            //     $message = "Player 2 is the Super Duper Freakin winner of the WHOLE ENTIRE Game!";
-            // }
+            if ($this->player1Win == 2)
+            {
+                $message = "Player 1 is the Super Duper Freakin winner of the WHOLE ENTIRE Game!";
+            } elseif ($this->player2Win == 2) {
+                $message = "Player 2 is the Super Duper Freakin winner of the WHOLE ENTIRE Game!";
+            }
 
             return $message;
         }
